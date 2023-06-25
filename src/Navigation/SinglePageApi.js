f5a905733826b1622da36771c82ae2c3cd7a9c2a
Chhavi Purbia <purbiachhavi11@gmail.com>
2023-06-26 00:14:10 +0530

    files added

--- src/Navigation/SinglePageApi.js
+++ src/Navigation/SinglePageApi.js
@@ -2,41 +2,41 @@ import React, { useState } from 'react';
 import './SinglePageApi.css';
 import { NavLink } from 'react-router-dom';
 import USRgenerate from '../USRgenerationFolder/USRgenerate';
-import viewTable from "../USRgenerationFolder/USR"
+import viewTable from "../USRgenerationFolder/USR";
+
 const App = () => {
   const [progress, setProgress] = useState(0);
   const [accordions, setAccordions] = useState({
     accordion1: false,
     accordion2: false,
-    accordion3: false,
-    accordion4: false,
   });
   const [accordion1Option, setAccordion1Option] = useState('');
   const [generatedContent, setGeneratedContent] = useState('');
+  const [originalSentences, setOriginalSentences] = useState([]);
+  const [modifiedSentences, setModifiedSentences] = useState([]);
 
   const handleButtonClick = (buttonIndex) => {
-    // Check if the previous accordion is opened
-    const previousAccordion = `accordion${buttonIndex - 1}`;
-    if (buttonIndex > 1 && !accordions[previousAccordion]) {
-      return; // Exit early if the previous step is not completed
-    }
-
     // Check if the button is already clicked
     const accordionKey = `accordion${buttonIndex}`;
-    if (accordions[accordionKey] || (buttonIndex === 2 && accordion1Option === '')) {
-      return; // Exit early if the button is already clicked or Accordion 1 option is not chosen
+    if (accordions[accordionKey]) {
+      return; // Exit early if the button is already clicked
     }
 
     // Update the progress based on the current accordion index
-    const newProgress = (buttonIndex / Object.keys(accordions).length) * 100;
+    const newProgress = (buttonIndex / 2) * 100;
     setProgress(newProgress);
 
-    // Open the clicked accordion and close the previous accordion
-    setAccordions((prevAccordions) => ({
-      ...prevAccordions,
-      [accordionKey]: true,
-      [previousAccordion]: false,
-    }));
+    // Open the clicked accordion and leave the other accordion state unchanged
+    setAccordions((prevAccordions) => {
+      if (buttonIndex === 2 && !prevAccordions.accordion1) {
+        return prevAccordions; // Exit early if the first accordion is not opened
+      }
+
+      return {
+        ...prevAccordions,
+        [accordionKey]: true,
+      };
+    });
   };
 
   const handleReset = () => {
@@ -44,11 +44,11 @@ const App = () => {
     setAccordions({
       accordion1: false,
       accordion2: false,
-      accordion3: false,
-      accordion4: false,
     });
     setAccordion1Option('');
     setGeneratedContent('');
+    setOriginalSentences([]);
+    setModifiedSentences([]);
   };
 
   const handleAccordion1OptionChange = (event) => {
@@ -56,20 +56,42 @@ const App = () => {
     setGeneratedContent('');
   };
 
+  const handleUSRGenerate = (content) => {
+    setGeneratedContent(content);
+    setOriginalSentences([]); // Clear previous data
+    setModifiedSentences([]); // Clear previous data
+
+    // Perform logic to generate original and modified sentences based on the generated content
+    // For demonstration purposes, let's assume the original and modified sentences are hardcoded
+    const original = ['Original Sentence 1', 'Original Sentence 2'];
+    const modified = ['Modified Sentence 1', 'Modified Sentence 2'];
+
+    setOriginalSentences(original);
+    setModifiedSentences(modified);
+
+    handleButtonClick(2); // Automatically open the second accordion after generating the content
+  };
+
   return (
     <div>
       <div className="button-container">
-        <button onClick={() => handleButtonClick(1)} className="button">
-          Enter Text
+        <button onClick={() => handleButtonClick(1)} className={`button ${accordions.accordion1 ? 'active' : ''}`}>
+          Text input
         </button>
-        <button onClick={() => handleButtonClick(2)} className="button">
-          Generate USR
-        </button>
-        <button onClick={() => handleButtonClick(3)} className="button">
-          Edit USR
-        </button>
-        <button onClick={() => handleButtonClick(4)} className="button">
-          Generate text
+        
+        
+        {accordions.accordion1 && ( // Render the "Generate USR" button only if accordion1 is opened
+          <button onClick={() => handleButtonClick(2)} className={`button ${accordions.accordion2 ? 'active' : ''}`} disabled={!generatedContent}>
+            Generate USR
+          </button>
+        )}
+        {accordions.accordion2 && ( // Render the "Edit the USR" button only if accordion2 is opened
+          <button className="button">
+            Edit the USR
+          </button>
+        )}
+        <button onClick={() => handleButtonClick(3)} className={`button ${accordions.accordion3 ? 'active' : ''}`} disabled={!accordions.accordion2}>
+          Results
         </button>
         <button onClick={handleReset} className="button">
           Reset
@@ -81,26 +103,41 @@ const App = () => {
       <div className="accordions">
         <h2>Language Communicator</h2>
         <div className="accordion">
-          <button onClick={() => handleButtonClick(1)}>Select Type of Text to be inserted</button>
+          <button onClick={() => handleButtonClick(1)} className='accordion-button'>Enter Text</button>
           {accordions.accordion1 && (
             <div>
-              <USRgenerate />
+              <USRgenerate onUSRGenerate={handleUSRGenerate} />
             </div>
           )}
         </div>
         <div className="accordion">
-        <button sx={{ margin: '5px' }} variant="contained" onClick={viewTable} >Edit</button>
+          <button onClick={() => handleButtonClick(2)}className='accordion-button'>Generate Hindi USR</button>
           {accordions.accordion2 && (
             <div>
-              
+              <div className="container">
+                <div className="box">
+                  <h2 className="box-heading">Original Hindi Sentences</h2>
+                  <ul>
+                    {originalSentences.map((sentence, index) => (
+                      <li key={index}>{sentence}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="box">
+                  <h2 className="box-heading">Generated Hindi Sentences</h2>
+                  <ul>
+                    {modifiedSentences.map((sentence, index) => (
+                      <li key={index}>{sentence}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <button onClick={handleReset} className="button" id="reset">
+                Reset (takes you to the initial phase of the process)
+              </button>
             </div>
           )}
         </div>
-        <div className="accordion">
-          <button onClick={() => handleButtonClick(3)}>Results</button>
-          {accordions.accordion3 && <div>Content for Accordion 3</div>}
-        </div>
-        
       </div>
     </div>
   );
